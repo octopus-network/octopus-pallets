887c51085c1ac2dfec847f664a5f890f00657040
five <five@0x11.email>
2022-09-21 00:35:39 +0800

    Set the default balance with premined + min .

--- bridge/src/lib.rs
+++ bridge/src/lib.rs
@@ -393,9 +393,10 @@ pub mod pallet {
 			let min = T::Currency::minimum_balance();
 			let amount =
 				self.premined_amount.checked_into().ok_or(Error::<T>::AmountOverflow).unwrap();
-			if amount >= min {
-				T::Currency::make_free_balance_be(&account_id, amount);
-			}
+
+			let init_amount = min + amont;
+			T::Currency::make_free_balance_be(&account_id, init_amount);
+
 			<OctopusPalletId<T>>::put(Some(account_id));
 			for (token_id, asset_id) in self.asset_id_by_token_id.iter() {
 				<AssetIdByTokenId<T>>::insert(token_id.as_bytes(), asset_id);
