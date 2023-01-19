d0e73abee57653303242a4b7fa1f8169f6cd2bd8
davirain <davirain.yin@gmail.com>
2023-01-19 16:13:36 +0800

    Update build.rs

--- appchain/build.rs
+++ appchain/build.rs
@@ -1,23 +1,21 @@
 use std::{env, fs::File, io::Write, path::Path, process::Command};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-	write_git_version()
+fn main() {
+	write_git_version();
 }
 
-fn write_git_version() -> Result<(), Box<dyn std::error::Error>> {
+fn write_git_version() {
 	let maybe_hash = get_git_hash();
 	let git_hash = maybe_hash.as_deref().unwrap_or("baaaaaad");
 
-	let dest_path = Path::new(&env::var("OUT_DIR")?).join("git_version");
+	let dest_path = Path::new(&env::var("OUT_DIR").unwrap()).join("git_version");
 
-	let mut file = File::create(&dest_path)?;
-	write!(file, "{}", git_hash)?;
+	let mut file = File::create(&dest_path).unwrap();
+	write!(file, "{}", git_hash).unwrap();
 
 	// TODO: are these right?
 	println!("cargo:rerun-if-changed=.git/HEAD");
 	println!("cargo:rerun-if-changed=.git/index");
-
-	Ok(())
 }
 
 fn get_git_hash() -> Option<String> {
